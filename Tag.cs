f05f155b9c49284d7fce395398e3195108216650
gustavo-machado-afterverse <gustavo-machado@afterverse.com>
2022-05-17 13:14:06 -0300

    Initial version, v1.0.0

---
+++ Tag.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace LegendaryTools.TagSystem
+{
+    [CreateAssetMenu(menuName = "Tools/Tag System/Tag")]
+    public class Tag : ScriptableObject
+    {
+        public string Id;
+        public string Name;
+        public string Category;
+        public string Description;
+    }
+}
